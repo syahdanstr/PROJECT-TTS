9bf6da73c1ac68d579363dc16890ab2e252b2ce1
syahdanstr <123710756+syahdanstr@users.noreply.github.com>
2023-03-09 21:38:54 +0700

    Update PROJECT.c

--- PROJECT.c
+++ PROJECT.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <conio.h>
+#include <windows.h>
+#include <math.h>
+#define MAX_KAMAR 15
+#define MAX_NAMA_LENGTH 50
+#define MAX_USERS 2
+#define MAX_USERNAME_LENGTH 20
+#define MAX_PASSWORD_LENGTH 20
